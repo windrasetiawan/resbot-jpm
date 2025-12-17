66522d021b60ea5ffc74088d02c8e13aff050d26
WindraSe <pemaingamez123@gmail.com>
2025-12-17 12:29:42 -0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,12 +1,18 @@
 async function menu(sock, sender, message) {
     const text = `
-╭❰ *MENU BOT* ❱
-│
-│ ➤ .autojpm
-│ ➤ .ping
-│ ➤ .addhc / .delhc
-│ ➤ .setopen / .setclose
-╰────────────`;
+╔═════════════════════╗
+║    🤖 *MENU BOT* ║
+╠═════════════════════╣
+║                     ║
+║ ➤ .autojpm          ║
+║ ➤ .ping             ║
+║ ➤ .listgc           ║
+║ ➤ .addhc / .delhc   ║
+║ ➤ .setopen / .close ║
+║                     ║
+╚═════════════════════╝
+`;
+    // Mengirim pesan balasan
     await sock.sendMessage(sender, { text: text });
 }
 export default menu;
