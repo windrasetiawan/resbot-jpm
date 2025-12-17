49e481b42b4f54ed786d519a4dc4bf0054d32ead
WindraSe <pemaingamez123@gmail.com>
2025-12-17 12:07:06 -0800

    Update autoreply.js

--- plugins/autoreply.js
+++ plugins/autoreply.js
@@ -1,12 +1,4 @@
-import fs from 'fs';
-
-// Ubah fungsi agar bisa di-export default
 async function autoreply(sock, sender, message, key, messageEvent) {
-    // Logika autoreply kamu disini
-    // Contoh sederhana:
-    if (message === 'ping') {
-        await sock.sendMessage(sender, { text: 'Pong!' });
-    }
+    // Logika autoreply
 }
-
 export default autoreply;
