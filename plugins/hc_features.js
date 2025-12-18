fa449fcfcc194c65d5a0881bf631a9b2fe3d8c13
WindraSe <pemaingamez123@gmail.com>
2025-12-18 21:33:34 +0700

    Update hc_features.js

--- plugins/hc_features.js
+++ plugins/hc_features.js
@@ -69,7 +69,7 @@ async function hcFeatures(sock, chatId, message, key, msg) {
                 document: fs.readFileSync(path.join(targetDir, matchFile)), 
                 mimetype: 'application/octet-stream', 
                 fileName: matchFile,
-                caption: `✅ Config: ${matchFile}`
+
             }, { quoted: msg });
         } else {
             return sock.sendMessage(chatId, { text: "❌ File tidak ditemukan." }, { quoted: msg });
@@ -175,7 +175,7 @@ async function hcFeatures(sock, chatId, message, key, msg) {
                 document: fs.readFileSync(path.join(targetDir, matchFile)), 
                 mimetype: 'application/octet-stream', 
                 fileName: matchFile,
-                caption: `✅ Config: ${matchFile}`
+
             }, { quoted: msg });
         }
     }
