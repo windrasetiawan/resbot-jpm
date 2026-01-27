43afc369c9593af423298d7e7f6c2dcb4ac42ff5
WindraSe <pemaingamez123@gmail.com>
2026-01-27 22:38:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -89,9 +89,8 @@ async function startBot() {
             ChangeStatus(__dirname + "/sessions/", "connected");
             resumeAutoJPM(sock);
             
-            // JALANKAN INTERVAL
             setInterval(() => { runGroupSchedule(sock); }, 60000);
-            startMonitor(sock); // START SERVER MONITOR
+            startMonitor(sock);
         }
     });
 
@@ -115,9 +114,23 @@ async function handleMsg(sock, msg) {
         try { db = JSON.parse(fs.readFileSync(settingsPath)); } catch {}
 
         if (db.autojoin && (text.includes("chat.whatsapp.com") || text.includes("wa.me"))) {
-            const code = text.match(/(?:chat\.whatsapp\.com\/|wa\.me\/)([0-9A-Za-z]{20,29})/);
-            if (code && code[1]) {
-                await sock.groupAcceptInvite(code[1]).catch(() => {});
+            const codeMatch = text.match(/(?:chat\.whatsapp\.com\/|wa\.me\/)([0-9A-Za-z]{20,29})/);
+            
+            if (codeMatch && codeMatch[1]) {
+                const inviteCode = codeMatch[1];
+                try {
+                    const groupInfo = await sock.groupGetInviteInfo(inviteCode);
+                    const groupName = groupInfo.subject ? groupInfo.subject.toUpperCase() : "";
+
+                    if (groupName.includes("VPN") || groupName.includes("CONFIG")) {
+                        console.log(clc.green(`✅ Auto Join: ${groupInfo.subject}`));
+                        await sock.groupAcceptInvite(inviteCode);
+                    } else {
+                        console.log(clc.yellow(`⚠️ Skip Join: ${groupInfo.subject}`));
+                    }
+                } catch (e) {
+                    console.log("Link invite invalid atau bot sudah bergabung.");
+                }
             }
         }
 
@@ -138,7 +151,7 @@ async function handleMsg(sock, msg) {
             tiktok(sock, chatId, text, msg.key, msg),
             owner(sock, chatId, text, msg.key, msg),
             igdl(sock, chatId, text, msg.key, msg),
-            serverMonitor(sock, chatId, text, msg.key, msg) // TAMBAHKAN PLUGIN MONITOR
+            serverMonitor(sock, chatId, text, msg.key, msg)
         ]);
 
     } catch (e) {
@@ -147,4 +160,3 @@ async function handleMsg(sock, msg) {
 }
 
 startBot();
-                                                  
